d7a93782a72158d0128268db9198cfdfe5964b05
Anirudh Upadhya K <64802718+AnirudhUK1999@users.noreply.github.com>
2023-12-29 18:26:34 +0530

    Operations of calculator implemented

--- js/functions.js
+++ js/functions.js
@@ -20,6 +20,8 @@ function divide(num1,num2){
 
 function operate(num1,operator,num2){
     let res = 0;
+    num1 = Number(num1)
+    num2 = Number(num2)
     operator = String(operator)
     
     if(operator == "+")
@@ -34,4 +36,60 @@ function operate(num1,operator,num2){
         res = divide(num1,num2);
     
     return res
+}
+
+
+// function showDisplay(){
+//     let numEvents = document.querySelectorAll('button');
+//     numEvents = Array.from(numEvents);
+//     numEvents.forEach(numEvent => 
+//         numEvent.addEventListener('click', () => {test(numEvent.textContent)}
+//         // val => 
+//         // {
+//         //     console.log();
+//         // }
+//         )
+//         );
+// }
+
+// function showDisplay(){
+    
+// }
+
+let numEvents = document.querySelectorAll('button');
+    numEvents.forEach(numEvent => numEvent.addEventListener('click',() => test(numEvent.textContent)))
+        // val => 
+        // {
+        //     console.log();
+        // }
+        ;
+
+let connect = "";
+function test(val){
+    let displayNum = document.querySelector('.display>.one-one');
+    let idk = 0
+    connect = connect.concat(val);
+    console.log("connect "+connect);
+    displayNum.textContent = connect;
+    let add = connect.indexOf("+")
+    let sub = connect.indexOf("-")
+    let mul = connect.indexOf("*")
+    let div = connect.indexOf("/")
+    idx = add * sub * mul * div;
+    if(val == "C"){
+        displayNum.textContent = null;
+        connect = "";
+    }
+    console.log(idx)
+    if(val == "=" && idx != 1){
+        idx *= -1;
+        let res = operate(connect.slice(0,idx),connect[idx],connect.slice(idx+1,connect.length-1));
+        console.log("res "+res);
+        displayNum.textContent = res;
+        connect = String(res);
+        console.log(idx)
+        
+    }
+        
+    // console.log('button pressed ' + val);
 }
--- js/helper.js
+++ js/helper.js
@@ -1,11 +1,4 @@
-function buttonCreation(){
-    const button = document.querySelector('.buttons-container')
-    for(let num = 0; num < 10; num++){
-        let calcButton = document.createElement('button');
-        calcButton.textContent = num
-        button.appendChild(calcButton);
-    }
-}
+
 
 // function displayCreate(){
 //     const display = document.querySelector('.display');
