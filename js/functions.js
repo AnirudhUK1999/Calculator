7e49e8486a521208e5e7e7e21cb0fb70d9ff7f94
Anirudh Upadhya K <64802718+AnirudhUK1999@users.noreply.github.com>
2024-01-04 21:53:13 +0530

    Mobile view is now cleaner

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,10 @@
+
+var siteWidth = 1280;
+var scale = screen.width /siteWidth;
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+
+
 function add(num1,num2){
     return num1+num2;
 }
